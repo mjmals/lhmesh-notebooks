2fa7d4847d87b320a39caf971c29f4ac9d61fe28
Matt Monks <mals2003@hotmail.com>
2023-11-22 23:25:41 +0000

    Added landing to raw notebook

--- synapse/git-clean.ps1
+++ synapse/git-clean.ps1
@@ -1,9 +1,3 @@
-foreach ($file in Get-ChildItem -Path "$PSScriptRoot\factory")
-{
-    Remove-Item -Path $file.FullName
-}
-
-foreach ($file in Get-ChildItem -Path "$PSScriptRoot\linkedService" -Filter "lsDomain*.json")
-{
-    Remove-Item -Path $file.FullName
-}
+Remove-Item -Path "$PSScriptRoot\credential" -Recurse
+Remove-Item -Path "$PSScriptRoot\integrationRuntime" -Recurse
+Remove-Item -Path "$PSScriptRoot\linkedService" -Recurse
